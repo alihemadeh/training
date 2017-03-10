a6df70440e27600e748e186900aa667bd81a51a7
Ali Hemadeh <alihemadeh@users.noreply.github.com>
2017-03-10 14:20:05 +0200

    Update index.php
    l num sar 2

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-      // author: Hassan Krayem
+      // author: Hassan Hrayem
       // description: file for testing multiple pull requests..
       
       public function HassanFN()
       {
         
-          $num = 1;
+          $num = 2;
           echo "Hassan $num";
 
       }
